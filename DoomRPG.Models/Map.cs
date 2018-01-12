ab05e85bfe45346416fb61ba361c5a5f2ec134fb
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2018-01-12 12:06:53 +0200

    Created the initial Models project

---
+++ DoomRPG.Models/Map.cs
@@ -0,0 +1,37 @@
+﻿using NuciXNA.Primitives;
+
+namespace DoomRPG.Models
+{
+    public sealed class Map
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        /// <value>The description.</value>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ceiling colour.
+        /// </summary>
+        /// <value>The ceiling colour.</value>
+        public Colour CeilingColour { get; set; }
+
+        /// <summary>
+        /// Gets or sets the floor colour.
+        /// </summary>
+        /// <value>The floor colour.</value>
+        public Colour FloorColour { get; set; }
+    }
+}
---
+++ DoomRPG.Models/Wall.cs
@@ -0,0 +1,29 @@
+﻿namespace DoomRPG.Models
+{
+    public sealed class Wall
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        /// <value>The description.</value>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the texture.
+        /// </summary>
+        /// <value>The texture.</value>
+        public string Texture { get; set; }
+    }
+}

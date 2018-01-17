3280360027c7510165b9f59825470c3640aa0abc
Horatiu Mlendea <mlendea.horatiu@gmail.com>
2018-01-17 17:45:07 +0200

    Basic camera

---
+++ DoomRPG.Gui/Camera.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace DoomRPG.Gui
+{
+    public class Camera
+    {
+        public Vector2 Position { get; set; }
+
+        public Vector2 Direction { get; set; }
+
+        public float Radius { get; set; }
+
+        public float MovementSpeed { get; set; }
+
+        public float RotationSpeed { get; set; }
+
+        public Camera()
+        {
+            Radius = 0.3f;
+
+            MovementSpeed = 3f;
+            RotationSpeed = 3f;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}

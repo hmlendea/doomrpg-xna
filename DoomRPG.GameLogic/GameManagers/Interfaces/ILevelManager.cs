6325a3fa50ec4897e18bffd50953f9653e03b02b
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2018-01-17 23:44:23 +0200

    Very basic level manager

---
+++ DoomRPG.GameLogic/GameManagers/Interfaces/ILevelManager.cs
@@ -0,0 +1,15 @@
+﻿using DoomRPG.Models;
+
+namespace DoomRPG.GameLogic.GameManagers.Interfaces
+{
+    public interface ILevelManager
+    {
+        /// <summary>
+        /// Gets the wall.
+        /// </summary>
+        /// <returns>The wall.</returns>
+        /// <param name="x">The X coordinate.</param>
+        /// <param name="y">The Y coordinate.</param>
+        WallInstance GetWall(int x, int y);
+    }
+}
---
+++ DoomRPG.GameLogic/GameManagers/LevelManager.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Linq;
+
+using DoomRPG.DataAccess.Repositories;
+using DoomRPG.GameLogic.GameManagers.Interfaces;
+using DoomRPG.GameLogic.Mapping;
+using DoomRPG.Models;
+using DoomRPG.Settings;
+
+namespace DoomRPG.GameLogic.GameManagers
+{
+    public class LevelManager : ILevelManager
+    {
+        Map currentLevel;
+
+        /// <summary>
+        /// Loads the content.
+        /// </summary>
+        /// <param name="id">Level identifier.</param>
+        public void LoadContent(string id)
+        {
+            string levelPath = Path.Combine(ApplicationPaths.EntitiesDirectory, "maps.xml");
+
+            MapRepository levelRepository = new MapRepository(levelPath);
+
+            currentLevel = levelRepository.Get(id).ToDomainModel();
+        }
+
+        /// <summary>
+        /// Gets the wall.
+        /// </summary>
+        /// <returns>The wall.</returns>
+        /// <param name="x">The X coordinate.</param>
+        /// <param name="y">The Y coordinate.</param>
+        public WallInstance GetWall(int x, int y)
+        {
+            return currentLevel.Walls.FirstOrDefault(wall => wall.Position.X == x && wall.Position.Y == y);
+        }
+    }
+}
